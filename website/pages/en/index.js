98520ef5abfbcba542c8163d2a42511c331d45c6
李杰 <lj706517903@163.com>
2019-09-20 14:10:14 +0800

    success website code review

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -65,8 +65,8 @@ class HomeSplash extends React.Component {
           <ProjectTitle siteConfig={siteConfig} />
           <PromoSection>
             <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+            <Button href={docUrl('doc2.html')}>GET START</Button>
+            <Button href={docUrl('Button.html')}>API</Button>
           </PromoSection>
         </div>
       </SplashContainer>
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -7,103 +7,111 @@
 
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
+const RemarkablePlugins = require('./core/RemarkablePlugins')
 
 // List of projects/orgs using your project for the users page.
 const users = [
-    {
-        caption: 'User1',
-        // You will need to prepend the image path with your baseUrl
-        // if it is not '/', like: '/test-site/img/image.jpg'.
-        image: '/img/undraw_open_source.svg',
-        infoLink: 'https://www.facebook.com',
-        pinned: true,
-    },
-]
+	{
+		caption: 'User1',
+		// You will need to prepend the image path with your baseUrl
+		// if it is not '/', like: '/test-site/img/image.jpg'.
+		image: '/img/undraw_open_source.svg',
+		infoLink: 'https://www.facebook.com',
+		pinned: true,
+	},
+];
 
 const siteConfig = {
-    title: 'Test Site', // Title for your website.
-    tagline: 'A website for testing',
-    url: 'https://your-docusaurus-test-site.com', // Your website URL
-    baseUrl: '/', // Base URL for your project */
-    // For github.io type URLs, you would set the url and baseUrl like:
-    //   url: 'https://facebook.github.io',
-    //   baseUrl: '/test-site/',
+	title: 'mCloud Design Mobile', // Title for your website.
+	tagline: '一个基于 React Native 的 UI 组件库',
+	url: 'https://your-docusaurus-test-site.com', // Your website URL
+	baseUrl: '/', // Base URL for your project */
+	// For github.io type URLs, you would set the url and baseUrl like:
+	//   url: 'https://facebook.github.io',
+	//   baseUrl: '/test-site/',
 
-    // Used for publishing and more
-    projectName: 'test-site',
-    organizationName: 'facebook',
-    // For top-level user or org sites, the organization is still the same.
-    // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-    //   organizationName: 'JoelMarcey'
+	// Used for publishing and more
+	projectName: 'test-site',
+	organizationName: 'facebook',
+	// For top-level user or org sites, the organization is still the same.
+	// e.g., for the https://JoelMarcey.github.io site, it would be set like...
+	//   organizationName: 'JoelMarcey'
 
-    // For no header links in the top nav bar -> headerLinks: [],
-    headerLinks: [
-        { doc: 'doc2', label: 'Docs' },
-        { doc: 'doc4', label: 'API' },
-        { page: 'help', label: 'Help' },
-        { blog: true, label: 'Blog' },
-    ],
+	// For no header links in the top nav bar -> headerLinks: [],
+	headerLinks: [
+		{ doc: 'doc2', label: 'Docs' },
+		{ doc: 'Button', label: 'API' },
+		{ page: 'help', label: 'Help' },
+		{ blog: true, label: 'Blog' },
+	],
 
-    // If you have users set above, you add it here:
-    users,
+	// If you have users set above, you add it here:
+	users,
 
-    /* path to images for header/footer */
-    headerIcon: 'img/favicon.ico',
-    footerIcon: 'img/favicon.ico',
-    favicon: 'img/favicon.ico',
+	/* path to images for header/footer */
+	headerIcon: 'img/favicon.ico',
+	footerIcon: 'img/favicon.ico',
+	favicon: 'img/favicon.ico',
 
-    /* Colors for website */
-    colors: {
-        primaryColor: '#5611ae',
-        secondaryColor: '#3c0b79',
-    },
+	/* Colors for website */
+	colors: {
+		primaryColor: '#586BFB',
+		secondaryColor: '#3c0b79',
+	},
 
-    /* Custom fonts for website */
-    /*
+	/* Custom fonts for website */
+	/*
   fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
+	myFont: [
+	  "Times New Roman",
+	  "Serif"
+	],
+	myOtherFont: [
+	  "-apple-system",
+	  "system-ui"
+	]
   },
   */
 
-    // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-    copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+	// This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+	copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
 
-    highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-        theme: 'default',
-    },
+	highlight: {
+		// Highlight.js theme to use for syntax highlighting in code blocks.
+		theme: 'default',
+	},
+	markdownPlugins: [
+		RemarkablePlugins.SnackPlayer,
+		// RemarkablePlugins.ReactNativeWebPlayer,
+	],
+	// Add custom scripts here that would be placed in <script> tags.
+	scripts: [
+		'https://snack.expo.io/embed.js',
+		'https://buttons.github.io/buttons.js',
+		'/js/codeblocks.js',
+	],
 
-    // Add custom scripts here that would be placed in <script> tags.
-    scripts: ['https://buttons.github.io/buttons.js'],
+	// On page navigation for the current documentation page.
+	onPageNav: 'separate',
+	// No .html extensions for paths.
+	cleanUrl: true,
 
-    // On page navigation for the current documentation page.
-    onPageNav: 'separate',
-    // No .html extensions for paths.
-    cleanUrl: true,
+	// Open Graph and Twitter card images.
+	ogImage: 'img/undraw_online.svg',
+	twitterImage: 'img/undraw_tweetstorm.svg',
+	// For sites with a sizable amount of content, set collapsible to true.
+	// Expand/collapse the links and subcategories under categories.
+	// docsSideNavCollapsible: true,
 
-    // Open Graph and Twitter card images.
-    ogImage: 'img/undraw_online.svg',
-    twitterImage: 'img/undraw_tweetstorm.svg',
-    // For sites with a sizable amount of content, set collapsible to true.
-    // Expand/collapse the links and subcategories under categories.
-    // docsSideNavCollapsible: true,
+	// Show documentation's last contributor's name.
+	// enableUpdateBy: true,
 
-    // Show documentation's last contributor's name.
-    // enableUpdateBy: true,
+	// Show documentation's last update time.
+	// enableUpdateTime: true,
 
-    // Show documentation's last update time.
-    // enableUpdateTime: true,
+	// You may provide arbitrary config keys to be used as needed by your
+	// template. For example, if you need your repo's URL...
+	//   repoUrl: 'https://github.com/facebook/test-site',
+};
 
-    // You may provide arbitrary config keys to be used as needed by your
-    // template. For example, if you need your repo's URL...
-    //   repoUrl: 'https://github.com/facebook/test-site',
-}
-
-module.exports = siteConfig
+module.exports = siteConfig;
