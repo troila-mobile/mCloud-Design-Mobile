bf227585034a8be4558f340f976a03eca543cf17
刘金萌 <1321839748ljm@gmail.com>
2019-09-23 10:12:10 +0800

    feat: SearchBar Checkbox Example

--- example/componentList.js
+++ example/componentList.js
@@ -3,5 +3,15 @@ export default [
 		title: 'Button',
 		description: '按钮',
 		example: require('../components/button/demo/basic')
+	},
+	{
+		title: 'SearchBar',
+		description: '搜索栏',
+		example: require('../components/searchBar/demo/basic')
+	},
+	{
+		title: 'Checkbox',
+		description: '复选框',
+		example: require('../components/checkbox/demo/basic')
 	}
 ]
