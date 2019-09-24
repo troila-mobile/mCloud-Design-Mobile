82e10859d7483a9bf6a8fea2a9a1bff3aa3ca838
sunmingli <sunmingli@troila.com>
2019-09-24 10:15:13 +0800

    no message

--- components/segmentedControl/demo/basic.js
+++ components/segmentedControl/demo/basic.js
@@ -3,16 +3,23 @@
  */
 import React from 'react'
 import { View } from 'react-native'
-// import {SegmentedControl} from '../..'
+import { SegmentedControl } from '../..'
 
 export default () => (
-    <View style={{ flex:1 }}>
-        {/*<SegmentedControl>default</SegmentedControl>*/}
-        {/*<SegmentedControl disabled>default disabled</SegmentedControl>*/}
-
-        {/*<SegmentedControl type="primary">primary</SegmentedControl>*/}
-        {/*<SegmentedControl type="primary" disabled>*/}
-            {/*primary disabled*/}
-        {/*</SegmentedControl>*/}
+    <View
+        style={{
+            flex:1,
+            alignItems:'center',
+            backgroundColor:'#e6e6e6'
+        }}
+    >
+        <SegmentedControl leftText='申请' rightText='审批' style={{ marginTop:20 }} />
+        <SegmentedControl disabled leftText='申请' rightText='审批' style={{ marginTop:20 }} />
+        <SegmentedControl
+            leftText='今日遭到榜'
+            rightText='昨日工时榜'
+            type='special'
+            style={{ marginTop:20,width:300,height:40 }}
+        />
     </View>
 )
