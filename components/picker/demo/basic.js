65ff1ec90f510a894430b177e55b558c8a66888d
李杰 <lj706517903@163.com>
2019-10-09 17:32:18 +0800

    调整用例

--- components/picker/demo/basic.js
+++ components/picker/demo/basic.js
@@ -3,7 +3,7 @@ import { Text, TouchableOpacity, View } from 'react-native'
 import { List, Picker } from '../..'
 import PropTypes from 'prop-types'
 
-const data = require('./data.json')
+const dataSource = require('./data.json')
 
 const CustomChildren = (props) => (
     <TouchableOpacity onPress={props.onPress}>
@@ -18,6 +18,7 @@ const CustomChildren = (props) => (
             <Text style={{ flex: 1 }}>{props.children}</Text>
             <Text style={{ textAlign: 'right', color: '#888', marginRight: 15 }}>
                 {props.extra}
+天
             </Text>
         </View>
     </TouchableOpacity>
@@ -30,50 +31,87 @@ CustomChildren.propTypes = {
 
 export default class extends React.Component {
     state = {
-        data: [],
         value: [],
+        data: [],
         pickerValue: [],
     }
-    onPress = () => {
-        setTimeout(() => {
-            this.setState({
-                data: [],
-            })
-        }, 500)
-    };
     onChange = (value) => {
         this.setState({ value })
     };
     render() {
+        const {
+            value,
+            data,
+            pickerValue,
+        } = this.state
         return (
             <View style={{ marginTop: 30 }}>
                 <List>
                     <Picker
-                        data={data}
+                        data={dataSource}
                         cols={3}
-                        value={this.state.value}
+                        value={value}
                         onChange={this.onChange}
                         title="Test-Title"
                     >
                         <List.Item arrow="horizontal">省市选择</List.Item>
                     </Picker>
                     <Picker
-                        data={this.state.data}
+                        data={[
+                            [{
+                                'label': '1',
+                                'value': '1',
+                            }, {
+                                'label': '2',
+                                'value': '2',
+                            }, {
+                                'label': '3',
+                                'value': '3',
+                            }],
+                            [{
+                                'label': 'a',
+                                'value': 'a',
+                            }, {
+                                'label': 'b',
+                                'value': 'b',
+                            }, {
+                                'label': 'c',
+                                'value': 'c',
+                            }],
+                        ]}
                         cols={2}
-                        value={this.state.value}
-                        onChange={this.onChange}
+                        value={data}
+                        onChange={(e) => {
+                            this.setState({ data:e })
+                        }}
+                        cascade={false}
                     >
-                        <List.Item arrow="horizontal" onPress={this.onPress}>
-                            省市选择(异步加载)
+                        <List.Item arrow="horizontal">
+                            非联动，双选
                         </List.Item>
                     </Picker>
                     <Picker
                         title="选择地区"
-                        data={[]}
-                        cols={2}
-                        value={this.state.pickerValue}
+                        data={[
+                            [{
+                                'label': '春',
+                                'value': '春',
+                            }, {
+                                'label': '夏',
+                                'value': '夏',
+                            }, {
+                                'label': '秋',
+                                'value': '秋',
+                            }, {
+                                'label': '冬',
+                                'value': '冬',
+                            }],
+                        ]}
+                        cols={1}
+                        value={pickerValue}
                         onChange={(v) => this.setState({ pickerValue: v })}
                         onOk={(v) => this.setState({ pickerValue: v })}
+                        cascade={false}
                     >
                         <CustomChildren>Customized children</CustomChildren>
                     </Picker>
