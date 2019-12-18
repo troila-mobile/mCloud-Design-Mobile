2c518abd41b3bc12405566ab8ecaed98f540d488
mxc7609 <xm350@nyu.edu>
2019-12-18 17:46:03 +0800

    no message

--- components/modal/loading.js
+++ components/modal/loading.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { portal } from './provider'
 import LoadingView from './LoadingView'
 
-let loadingKey = null
+const loadingKey = 'mCloudLoadingViewKey'
 export function showLoading(
     text,
 ) {
-    if (loadingKey) return
-    loadingKey = portal.add(
+    portal.update(
+        loadingKey,
         <LoadingView
             text={text}
         />
@@ -15,8 +15,5 @@ export function showLoading(
 }
 
 export function hideLoading() {
-    if (loadingKey) {
-        portal.remove(loadingKey)
-        loadingKey = null
-    }
+    portal.remove(loadingKey)
 }
